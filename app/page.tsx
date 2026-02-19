160372fc964fcaf00acad1e669f95353e0156dfc
Chris Klemz <crklemz@gmail.com>
2026-02-18 20:29:49 -0600

    style: faster side scroll gallery

--- app/page.tsx
+++ app/page.tsx
@@ -183,13 +183,13 @@ export default function HomePage() {
                 imagePaths={landingImagePaths.slice(0, 6)}
                 altPrefix={galleryAltPrefix}
                 className="mb-4"
-                scrollDurationSeconds={70}
+                scrollDurationSeconds={25}
                 direction="ltr"
               />
               <ScrollGallery
                 imagePaths={landingImagePaths.slice(6, 11)}
                 altPrefix={galleryAltPrefix}
-                scrollDurationSeconds={70}
+                scrollDurationSeconds={25}
               />
             </div>
           </div>
